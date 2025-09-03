085b1aa3fb072e7726f3b3e97d377e79616b3004
khaes-kth <khaes@kth.se>
2025-09-03 18:26:02 +0200

    Revert "fix: made deployable to cloudflare"
    This reverts commit 09b382c9368c0c3e5588a82585de2c4b034d499d.

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+const isProjectPage = true;
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
-  },
-  publicDir: 'public',
+  base: isProjectPage ? '/projection-boxes/' : '/',
 })
